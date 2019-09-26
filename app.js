f4ebe45d73b0d1b409e571869da38e8d8fa45d53
WhalenSITHS <michael.whalen@sitechhs.com>
2019-09-26 11:45:50 -0400

    commit removing avenger

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const avengers = [];
 
 
 //create individual avengers ** Objects function constructor
-function Avenger(alias, identity, powers, firstAppearance){
+/* function Avenger(alias, identity, powers, firstAppearance){
     this.alias = alias;
     this.identity = identity;
     this.powers = powers;
@@ -13,14 +13,52 @@ function Avenger(alias, identity, powers, firstAppearance){
     this.yearsActive = function(){
         return new Date().getUTCFullYear() - this.firstAppearance;
     }
-}
+} */
 
 //const ironMan = new Avenger('Iron-Man', 'Tony Stark', 'Smart, Facial Hair, Dead', 1963);
 //console.log(ironMan);
 
 //push into array
-function addAvenger(alias, identity, powers, firstAppearance){
+/* function addAvenger(alias, identity, powers, firstAppearance){
     avengers.push(new Avenger(alias, identity, powers, firstAppearance));
-}
-addAvenger('Captain America', 'Steve Rogers', 'Freedom', 1941);
+} */
+//addAvenger('Captain America', 'Steve Rogers', 'Freedom', 1941);
 //display obejcts on screen
+
+const form = document.querySelector('#hero-form'); //could also getElementById
+const userAlias = document.querySelector('#alias');
+const userPowers = document.querySelector('#powers');
+const userFullName = document.querySelector('#full-name');
+const userFirstAppearance = document.querySelector('#first-appearance');
+const userImage = document.querySelector('#image');
+const display = document.querySelector('.display');
+
+function eventListeners(){
+    form.addEventListener('submit', displayAvengerMember);
+    display.addEventListener('click', removeAvenger);
+    /* form.addEventListener('submit', function(){
+       console.log('test');
+   });  */
+}
+eventListeners();
+
+function displayAvengerMember(e) {
+    let html = '<div class="display-avenger"><div class="display-alias">%alias%</div><div class="display-powers">%powers%</div><div class="display-name">%name%</div><div class="first-appearance">%years%</div><div class="display-image"><img src="%url%" alt=""></div><div class="remove-avenger"><p class="remove-avenger">Remove Avenger &#10006; </p></div></div>';
+
+    let newHtml = html.replace('%alias%', userAlias.value);
+    newHtml = newHtml.replace('%powers%', userPowers.value);
+    newHtml = newHtml.replace('%name%', userFullName.value);
+    newHtml = newHtml.replace('%years%', userFirstAppearance.value);
+    newHtml = newHtml.replace('%url%', userImage.value);
+    display.insertAdjacentHTML('beforeend', newHtml);
+    //console.log('is the vebnt firing?');
+    e.preventDefault();
+
+}
+
+function removeAvenger(e) {
+    if(e.target.parentElement.classList.contains('remove-avenger')){
+        //e.target.parentElement.parentElement.remove();
+        console.log(e.target.parentElement);
+    }
+}
